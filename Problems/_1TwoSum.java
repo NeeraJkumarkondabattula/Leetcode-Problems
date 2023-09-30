b4a85aa970934e7b1b2519dbcb882d3078cd45e2
Neeraj Kumar Kondabattula <119170814+NeeraJkumarkondabattula@users.noreply.github.com>
2023-09-30 20:14:09 +0530

    Update _1TwoSum.java
    First Problem Two Sum

--- Problems/_1TwoSum.java
+++ Problems/_1TwoSum.java
@@ -1,7 +1,26 @@
 import java.util.*;
 
-public class _1TwoSum {
-    public static void main(String[] args) {
+class _1TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
+            }
+            map.put(nums[i], i);
+        }
+        return nums;
+    }
 
+    public String print(int[] arr) {
+        return Arrays.toString(arr);
+    }
+
+    public static void main(String[] args) {
+        _1TwoSum obj = new _1TwoSum();
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int target = 6;
+        int[] ans = obj.twoSum(arr, target);
+        System.out.println(obj.print(ans));
     }
 }
